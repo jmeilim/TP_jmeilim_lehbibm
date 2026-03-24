e66b577b65c93fb5bb6c30331ef74101672a9332
lehbib <mohamedsaidlehbib@gmail.com>
2026-03-24 18:07:04 +0100

    commentaire Lab4

--- src/Lab4.cxx
+++ src/Lab4.cxx
@@ -2,17 +2,27 @@
 #include <fstream>
 #include <cmath>
 
+
+
+/**
+ * @brief Potentiel de Lennard-Jones
+ */
 double U(double r, double eps = 1, double sig = 1){
     return 4 * eps * pow(sig/r,6) *( pow(sig/r,6)-1);
 }
 
+
+/**
+ * @brief claase pour representer l'univers de simulation
+ */
 class Univers {
 public:
-    double eps ;
-    double sig;
-    double dim ;
-    double ld;
-    double rc;
+    double eps;   ///< profondeur du potentiel
+    double sig;  ///< distance caractéristique
+    int  dim;     ///< dimension 
+    double ld;    ///< taille de la boîte
+    double rc;   ///< rayon de coupure
+    
     Univers(double e, double s, int dimension, double l, double rcut)
         : eps(e), sig(s), dim(dimension), ld(l), rc(rcut) {}
 };
